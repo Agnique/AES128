ae217f2e50b1c0834d592534b8e802359ca10cd0
Agnique <ccaizi@icloud.com>
2019-06-01 16:20:00 +0800

    initial commit

---
+++ aes128.hpp
@@ -0,0 +1,44 @@
+//
+//  aes128.hpp
+//  AES128
+//
+//  Created by 蔡佳仪 on 2019/5/31.
+//  Copyright © 2019 CaiJY. All rights reserved.
+//
+
+#ifndef aes128_hpp
+#define aes128_hpp
+
+#include <stdio.h>
+#include <vector>
+#endif /* aes128_hpp */
+
+typedef std::vector<unsigned char> ByteArray;
+
+#define BLOCK_SIZE 16
+#define KEY_SIZE 16
+#define NB 4
+#define NR 10
+
+class aes128
+{
+    public:
+        aes128(const ByteArray& key);
+        //~aes128();
+    
+        void encrypt(const ByteArray &pt, ByteArray &sm);
+        //void decrypt(const ByteArray &sm, ByteArray &pt);
+    private:
+        unsigned char m_state[4][NB];
+        unsigned char m_rkey[(NR+1)*4][4];
+        ByteArray m_key;
+        void AddRoundKey(const int round);
+        void KeyExpansion();
+        void SubBytes();
+        void Shiftrows();
+        void MixColumns();
+    
+        void get_state();
+    
+    
+};
---
+++ main.cpp
@@ -0,0 +1,126 @@
+//
+//  main.cpp
+//  AES128
+//
+//  Created by 蔡佳仪 on 2019/5/31.
+//  Copyright © 2019 CaiJY. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "aes128.hpp"
+using namespace std;
+
+void ReadHex(ifstream &fin, ByteArray &value, const int size);
+
+int main() {
+    
+    //FILE *fp_req, *fp_rsp;
+    //unsigned char pt[BLOCK_SIZE];  // plain text
+    //unsigned char key[KEY_SIZE];   // key
+    //unsigned char sm[BLOCK_SIZE];  // secret message
+    
+    ByteArray key(KEY_SIZE);
+    ByteArray pt(BLOCK_SIZE);
+    ByteArray sm(BLOCK_SIZE);
+    string str;
+    ifstream inFile;
+    ofstream outFile;
+    
+    /* === reading from .req file === */
+    inFile.open("AES128_KAT.req");
+    if(!inFile.is_open())
+    {
+        cout<<"Could not open file."<<endl;
+        cout<<"Programe terminating. "<<endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    getline(inFile, str);
+    if(str != "key:")
+    {
+        cout<<"Please specify encryption key."<<endl;
+        exit(EXIT_FAILURE);
+    }
+    ReadHex(inFile, key, KEY_SIZE);
+    
+    getline(inFile, str);
+    getline(inFile, str);
+    if(str != "plain text:")
+    {
+        cout<<"Please specify plain text."<<endl;
+        exit(EXIT_FAILURE);
+    }
+    ReadHex(inFile, pt, BLOCK_SIZE);
+
+    
+    
+    
+    /* === Encryption === */
+    aes128 cipher(key);
+    cipher.encrypt(pt, sm);
+    
+    /* === writing to .rsp file === */
+    outFile.open("AES128_KAT.rsp");
+    str = "key:";
+    outFile << str << endl;
+    for(int i=0; i<BLOCK_SIZE; i++)
+    {
+        outFile << hex << setfill('0') << setw(2) << int(key[i]);
+        
+    }
+    outFile << endl;
+    str = "plain text:";
+    outFile << str << endl;
+    for(int i=0; i<BLOCK_SIZE; i++)
+    {
+        outFile << hex << setfill('0') << setw(2) << int(pt[i]);
+        
+    }
+    outFile << endl;
+    str = "secret message:";
+    outFile << str << endl;
+    for(int i=0; i<BLOCK_SIZE; i++)
+    {
+        outFile << hex << setfill('0') << setw(2) << int(sm[i]);
+        
+    }
+    outFile << endl;
+    
+    inFile.close();
+    outFile.close();
+    
+    return 0;
+}
+
+/* Read hex value from file */
+void ReadHex(ifstream &fin, ByteArray &value, const int size)
+{
+    unsigned char t1, t2;
+    
+    for(int i=0; i<size; i++)
+    {
+        fin >> t1;
+        if('0'<= t1 && '9'>=t1)
+            t1 = 0 + t1 - '0';
+        else if('a'<=t1 && 'f'>=t1)
+            t1 = 10 + t1 - 'a';
+        else if('A'<=t1 && 'F'>=t1)
+            t1 = 10 + t1 - 'A';
+        else
+            t1 = 0; // shouldn't get here
+        
+        fin >> t2;
+        if('0'<= t2 && '9'>=t2)
+            t2 = 0 + t2 - '0';
+        else if('a'<=t2 && 'f'>=t2)
+            t2 = 10 + t2 - 'a';
+        else if('A'<=t1 && 'F'>=t1)
+            t2 = 10 + t2 - 'A';
+        else
+            t2 = 0; // shouldn't get here
+        
+        value[i] = (t1<<4)+t2;
+    }
+}
